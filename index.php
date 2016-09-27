0a81ddf5c1256977da523b94d4391b1b46732f70
manel <manelgavalda@iesebre.com>
2016-09-27 17:28:50 +0200

    Added namespaces

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
 
+use HelloWorld\SayHello;
+use manelgavalda\SayHello as SayHello2;
+
 //Per eliminar tots els requires i usarem el autoload perquè ens carregui tots els fitxers.
 require "vendor/autoload.php";
+require "src/SayHello.php";
+
 
 //$sayhello= new \HelloWorld\SayHello();
 //echo $sayhello->world();
 
-//Versió estàtica
-echo \HelloWorld\SayHello::world();
+//Versió estàtica.
+//echo \HelloWorld\SayHello::world();
+echo SayHello::world();
+//Gràcies al namespace podem cridar les 2 classes de forma diferent usant el prefix.
+//echo \manelgavalda\SayHello::world();
+echo SayHello2::world();
---
+++ src/SayHello.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manel
+ * Date: 27/09/16
+ * Time: 17:18
+ */
+// Li afegim per exemple el nostre nom d'usuari com a namespace.
+namespace manelgavalda;
+
+class SayHello
+{
+    public static function world()
+    {
+        return "Hola mon!";
+    }
+}
